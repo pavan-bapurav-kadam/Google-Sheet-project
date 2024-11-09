39ccb84928a6e5608cbbb3ee3d5d0bd0489ba72b
pavan-bapurav-kadam <pavankadam141@gmail.com>
2024-11-09 10:27:18 +0530

    New Changes

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
-const scriptURL = 'https://script.google.com/macros/s/AKfycbzypF-et24W_xH_grtt9xtEP353NgUgbVeJoEJPS4lZlPpLasGqSdGlSYl4c1W6gYN-/exec'
-const form = document.forms['product']
+var scriptURL = 'https://script.google.com/macros/s/AKfycbzypF-et24W_xH_grtt9xtEP353NgUgbVeJoEJPS4lZlPpLasGqSdGlSYl4c1W6gYN-/exec'
+
+var form = document.forms['product']
 
 form.addEventListener('submit', e => {
     e.preventDefault()
-    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+    fetch(scriptURL, { method: 'POST', body: new FormData(form),mode: 'no-cors'})
     .then(response => alert("Thank you! your form is submitted successfully." ))
     .then(() => {  window.location.reload(); })
     .catch(error => console.error('Error!', error.message))
